66f9d76f07cf537937b79912d22c0ba4231219e7
Kou-JunHao <koukunhao@github.com>
2026-03-31 20:46:11 +0800

    Add twrp_myron product for OrangeFox build

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,18 @@
 #
 # Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2026 OrangeFox Recovery Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_myron.mk
+    $(LOCAL_DIR)/omni_myron.mk \
+    $(LOCAL_DIR)/twrp_myron.mk
 
 COMMON_LUNCH_CHOICES := \
     omni_myron-user \
     omni_myron-userdebug \
-    omni_myron-eng
+    omni_myron-eng \
+    twrp_myron-user \
+    twrp_myron-userdebug \
+    twrp_myron-eng
---
+++ twrp_myron.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 OrangeFox Recovery Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit from myron device
+$(call inherit-product, device/xiaomi/myron/device.mk)
+
+# Inherit some common OrangeFox stuff.
+$(call inherit-product, vendor/fox/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := myron
+PRODUCT_NAME := twrp_myron
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi 25102RKBEC (myron)
+PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_RELEASE_NAME := Redmi myron
